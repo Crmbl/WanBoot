526aba3ac9898229753f9f2c84ec3c1696b9b63a
CrmbL <axelsch@hotmail.fr>
2018-03-04 23:14:58 +0100

    Major update
    added design
    added methods to ping and boot
    added popup to get token
    -- not working because of https need to find out how to do it

--- app/src/main/java/com/schaeffer/axel/wanboot/MainActivity.kt
+++ app/src/main/java/com/schaeffer/axel/wanboot/MainActivity.kt
@@ -1,34 +1,332 @@
 package com.schaeffer.axel.wanboot
 
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.ProgressBar
 import android.widget.TextView
+import com.github.ybq.android.spinkit.style.Circle
+import org.jetbrains.anko.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.util.*
+import java.util.stream.Collectors
+import kotlin.concurrent.timerTask
+import kotlinx.android.synthetic.main.login_popup.view.*
+import javax.net.ssl.HttpsURLConnection
 
 class MainActivity : AppCompatActivity() {
+    private var usernameApi : String = ""
+    private var urlServer : String = ""
+    private var passwordApi : String = ""
+    private var token : String = ""
+    private var notNeeded : Boolean = false
+    private var alert : AlertDialogBuilder? = null
+    private var isChecked : Boolean = false
+    private var prefs : SharedPreferences? = null
+
+    private val prefsFileName = "com.schaeffer.axel.wanboot.prefs"
+    private val urlApiPing : String = "/api/wan/Ping"
+    private val urlApiBoot : String = "/api/wan/BootUp"
+    private val urlApiToken : String = "/api/token/"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
     }
 
-    fun onClickBtn(v: View) {
-        var label = findViewById<TextView>(R.id.label_status)
-        var info = findViewById<TextView>(R.id.label_info)
-        if (label.text == "Wake me up!")
-        {
-            info.visibility = View.VISIBLE
-            info.text = "*contacting webservices" + "\n" + "*OKK" + "\n" + "*sending magic packet" + "\n"
+    override fun onResume() {
+        super.onResume()
 
-            label.text = "I'm awake!"
-            label.setTextAppearance(R.style.TextStatusThemeAwake)
+        prefs = this.getSharedPreferences(prefsFileName, 0)
+        usernameApi = prefs!!.getString("usernameApi", "")
+        passwordApi = prefs!!.getString("passwordApi", "")
+        urlServer = prefs!!.getString("urlServer", "")
+        isChecked = prefs!!.getBoolean("isChecked", false)
+
+        alert = alert {
+            this.customView{
+                val input : View = layoutInflater.inflate(R.layout.login_popup, null)
+                this.addView(input, null)
+                if (usernameApi != "")
+                    input.editText.setText(usernameApi)
+                if (passwordApi != "")
+                    input.editText2.setText(passwordApi)
+                if (urlServer != "")
+                    input.editText3.setText(urlServer)
+                if (isChecked == true)
+                    input.checkBox.isChecked = true
+
+                input.button.setOnClickListener { onClickLogin(input) }
+                input.checkBox.setOnCheckedChangeListener { _, b ->  onCheckBoxChecked(b) }
+            }
+        }.apply {
+            cancellable(false)
+            setFinishOnTouchOutside(false)
+        }.show()
+    }
+
+    private fun setToken() {
+        notNeeded = false
+        var info = findViewById<TextView>(R.id.label_info)
+        var label = findViewById<TextView>(R.id.label_status)
+        var button = findViewById<Button>(R.id.push_button)
+
+        var result = ""
+        try {
+            doAsync(task = {
+                val urlConnection = URL("$urlServer$urlApiToken").openConnection() as HttpsURLConnection
+                urlConnection.requestMethod = "POST"
+                urlConnection.connectTimeout = 10000
+                urlConnection.setRequestProperty("Content-Type","application/json")
+                val str = "{\"username\": \"$usernameApi\", \"password\": \"$passwordApi\"}"
+                val outputInBytes = str.toByteArray(charset("UTF-8"))
+                val os = urlConnection.outputStream
+                os.write(outputInBytes)
+                os.close()
+
+                val responseCode = urlConnection.responseCode
+                result = BufferedReader(InputStreamReader(urlConnection.inputStream)).lines().collect(Collectors.joining())
+                urlConnection.disconnect()
+
+                if (!notNeeded) {
+                    if (responseCode == 200) {
+                        uiThread {
+                            token = result.split(':')[1].split('"')[1]
+                            hideLoader()
+                            pingRequest(true)
+                        }
+                    } else {
+                        uiThread {
+                            info.text = "${formatPrompt(getString(R.string.server_not_responding))}"
+                            info.text = "${formatPrompt(getString(R.string.status_error))}"
+                            label.text = getString(R.string.title_error)
+                            label.setTextAppearance(R.style.TextStatusThemeError)
+                            button.setBackgroundResource(R.drawable.button_bg_round_diabled)
+                            button.isClickable = false
+
+                            hideLoader()
+                        }
+                    }
+                }
+            })
         }
-        else
-        {
-            info.visibility = View.INVISIBLE
-            label.text = "Wake me up!"
-            label.setTextAppearance(R.style.TextStatusThemeSleeping)
+        finally {
+            val timer = Timer()
+            timer.schedule(timerTask {
+                if (result == "") {
+                    runOnUiThread {
+                        info.text = "${formatPrompt(getString(R.string.server_not_responding))}"
+                        info.text = "${formatPrompt(getString(R.string.status_error))}"
+                        label.text = getString(R.string.title_error)
+                        label.setTextAppearance(R.style.TextStatusThemeError)
+                        button.setBackgroundResource(R.drawable.button_bg_round)
+                        button.isClickable = true
+                        hideLoader()
+                    }
+                }
+
+                notNeeded = true
+            }, 10000)
         }
     }
+
+    private fun sendMagicPackage() {
+        notNeeded = false
+        var info = findViewById<TextView>(R.id.label_info)
+        var label = findViewById<TextView>(R.id.label_status)
+        var button = findViewById<Button>(R.id.push_button)
+
+        info.text = "${formatPrompt("${getString(R.string.status_magic)} $urlServer...")}"
+
+        var result = ""
+        try
+        {
+            doAsync {
+                val urlConnection = URL("$urlServer$urlApiBoot").openConnection() as HttpsURLConnection
+                urlConnection.requestMethod = "GET"
+                urlConnection.setRequestProperty("Authorization", "Bearer $token")
+                val responseCode = urlConnection.responseCode
+                val result = BufferedReader(InputStreamReader(urlConnection.inputStream)).lines().collect(Collectors.joining())
+                urlConnection.disconnect()
+
+                //val result = URL(URL_API_WAN + BOOT_REQUEST).readText()
+                uiThread {
+                    if (!notNeeded) {
+                        if (result == "Success" && responseCode == 200) {
+                            info.text = "${formatPrompt(getString(R.string.magic_sparkle))}"
+                            pingRequest(false)
+                        }
+                        else {
+                            info.text = "${formatPrompt(getString(R.string.error_magic))}"
+                            info.text = "${formatPrompt(getString(R.string.status_chaos))}"
+                        }
+                    }
+                }
+            }
+        }
+        finally {
+            val timer = Timer()
+            timer.schedule(timerTask {
+                if (result == "") {
+                    runOnUiThread {
+                        info.text = "${formatPrompt(getString(R.string.server_not_responding))}"
+                        info.text = "${formatPrompt(getString(R.string.status_error))}"
+                        label.text = getString(R.string.title_error)
+                        label.setTextAppearance(R.style.TextStatusThemeError)
+                        button.setBackgroundResource(R.drawable.button_bg_round)
+                        button.isClickable = true
+                    }
+                }
+
+                notNeeded = true
+            }, 10000)
+        }
+    }
+
+    private fun pingRequest(justPing : Boolean) {
+        notNeeded = false
+        var label = findViewById<TextView>(R.id.label_status)
+        var info = findViewById<TextView>(R.id.label_info)
+        var button = findViewById<Button>(R.id.push_button)
+
+        label.text = getString(R.string.title_determining)
+        label.setTextAppearance(R.style.TextStatusThemeDetermining)
+        info.text = "${formatPrompt("${getString(R.string.status_ping)} $urlServer...")}"
+        button.setBackgroundResource(R.drawable.button_bg_round_diabled)
+        button.isClickable = false
+
+        var result = ""
+        try {
+            doAsync {
+                val urlConnection = URL("$urlServer$urlApiPing").openConnection() as HttpsURLConnection
+                urlConnection.requestMethod = "GET"
+                urlConnection.setRequestProperty("Authorization", "Bearer $token")
+                val responseCode = urlConnection.responseCode
+                result = BufferedReader(InputStreamReader(urlConnection.inputStream)).lines().collect(Collectors.joining())
+                urlConnection.disconnect()
+
+                uiThread {
+                    if (!notNeeded) {
+                        if (result == "Success" && responseCode == 200) {
+                            info.text = "${formatPrompt(getString(R.string.hello_world))}"
+                            info.text = "${formatPrompt(getString(R.string.status_awake))}"
+                            label.text = getString(R.string.title_awake)
+                            label.setTextAppearance(R.style.TextStatusThemeAwake)
+
+                            if (justPing) {
+                                button.setBackgroundResource(R.drawable.button_bg_round)
+                                button.isClickable = true
+                            } else {
+                                button.setBackgroundResource(R.drawable.button_bg_round_diabled)
+                                button.isClickable = false
+                            }
+                        }
+                        else {
+                            info.text = "${formatPrompt(getString(R.string.sleep_zzz))}"
+                            info.text = "${formatPrompt(getString(R.string.status_sleep))}"
+                            label.text = getString(R.string.title_asleep)
+                            label.setTextAppearance(R.style.TextStatusThemeSleeping)
+                            button.setBackgroundResource(R.drawable.button_bg_round)
+                            button.isClickable = true
+                        }
+                    }
+                }
+            }
+        }
+        finally {
+            val timer = Timer()
+            timer.schedule(timerTask {
+                if (result == "") {
+                    runOnUiThread {
+                        info.text = "${formatPrompt(getString(R.string.server_not_responding))}"
+                        info.text = "${formatPrompt(getString(R.string.status_error))}"
+                        label.text = getString(R.string.title_error)
+                        label.setTextAppearance(R.style.TextStatusThemeError)
+                        button.setBackgroundResource(R.drawable.button_bg_round)
+                        button.isClickable = true
+                    }
+                }
+
+                notNeeded = true
+            }, 10000)
+        }
+    }
+
+    //region UI methods
+
+    private fun onClickLogin(v : View) {
+        usernameApi = v.editText.text.toString()
+        passwordApi = v.editText2.text.toString()
+        urlServer = v.editText3.text.toString()
+
+        if (prefs != null) {
+            val editor = prefs!!.edit()
+            if (isChecked) {
+                editor.putString("usernameApi", usernameApi)
+                editor.putString("passwordApi", passwordApi)
+                editor.putString("urlServer", urlServer)
+                editor.putBoolean("isChecked", true)
+            }
+            else {
+                editor.clear()
+            }
+
+            editor.apply()
+        }
+
+        alert?.dismiss()
+        displayLoader()
+        setToken()
+    }
+
+    private fun onCheckBoxChecked(b : Boolean) {
+        isChecked = b
+    }
+
+    fun onClickBtn(v: View) {
+        // send magic package to boot the computer.
+        sendMagicPackage()
+    }
+
+    private fun formatPrompt(stringToAdd : String) : String{
+        var info = findViewById<TextView>(R.id.label_info)
+        val textSplit = info.text.split('\n')
+
+        if (textSplit.size > 10) {
+            info.text = ""
+            for ((index, value) in textSplit.withIndex()) {
+                if (index == 0)
+                    continue
+                if (value == "")
+                    continue
+
+                info.text = "${info.text}$value\n"
+            }
+        }
+
+        info.text = "${info.text}$stringToAdd\n"
+        return info.text.toString()
+    }
+
+    private fun displayLoader() {
+        var gridLoader : GridLayout = this@MainActivity.findViewById(R.id.GridLoader)
+        var loader : ProgressBar = gridLoader.findViewById(R.id.spin_kit)
+
+        loader?.indeterminateDrawable = Circle()
+        gridLoader?.visibility = View.VISIBLE
+    }
+
+    private fun hideLoader() {
+        var gridLoader : GridLayout = this@MainActivity.findViewById(R.id.GridLoader)
+        var loader : ProgressBar = gridLoader.findViewById(R.id.spin_kit)
+
+        gridLoader?.visibility = View.INVISIBLE
+    }
+
+    //endregion UI methods
 }
